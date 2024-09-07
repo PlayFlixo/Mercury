64afc17e1cb7987c6346a5095d5c573eb912b5fb
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-09-07 18:31:31 -0500

    Update movie.js

--- public/movie.js
+++ public/movie.js
@@ -6,7 +6,7 @@ input.addEventListener("keydown", function(event){
     if(event.keyCode === 13){
         var value = input.value.toString();
         if(value.includes("tt")){
-            var fullURL = "https://autoembed.to/movie/imdb/"+value;
+            var fullURL = "https://autoembed.co/movie/imdb/"+value;
             console.log(fullURL)
             embed.src=window.location.href.replace('/movie.html','')+`/mercury/?url=${btoa(fullURL)}`
         }
